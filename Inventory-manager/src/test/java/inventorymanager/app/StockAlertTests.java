45d768881ff8471d51c364f2f359934a470d5689
zotzo <hhhjejuas@gmail.com>
2026-02-26 18:55:24 +0100

    file for stock alert tests

--- Inventory-manager/src/test/java/inventorymanager/app/StockAlertTests.java
+++ Inventory-manager/src/test/java/inventorymanager/app/StockAlertTests.java
@@ -1,1 +1,13 @@
-// add
+package invetorymanager.app.Product;
+import inentorymanager.app.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("tests for stock alert")
+
+class StockAlertTests {
+    @Test
+    
+}
