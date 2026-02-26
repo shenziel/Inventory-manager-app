96d371b4173d6a3036e666de2acbbb72bd6efde8
zotzo <hhhjejuas@gmail.com>
2026-02-26 23:13:42 +0100

    fixed typos

--- Inventory-manager/src/test/java/inventorymanager/app/StockAlertTests.java
+++ Inventory-manager/src/test/java/inventorymanager/app/StockAlertTests.java
@@ -1,5 +1,6 @@
 package inventorymanager.app.services;
 import inventorymanager.app.Product;
+import inventorymanager.app.services.InventoryService;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,21 +9,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("tests for stock alert")
 
 public class StockAlertTests {
-    private InventoryService inventoryService = new TestInventoryService(15);
+    private InventoryService inventoryService = new InventoryService();
 
     @Test
     void testStockAlert_returnsTrueWhenProductIsBelowStock() {
         Product product = new Product("hg01", "gundam", 10);
-       InventoryService inventoryService addProduct(product, 10);
-        boolean stock = InventoryService.isLowStock(product);
+        inventoryService.addProduct(product, 10);
+        boolean stock = inventoryService.isLowStock(product);
         assertTrue(stock);
     }
 
     @Test
     void testStockAlert_returnsFalseWhenProductIsAboveStock() {
         Product product new Product("hg02", "gundam age-FX", 15);
-        inventoryService addProduct(product, 20);
-        boolean stock = InventoryService.isLowStock(product);
-        assertfalse(stock);
+        inventoryService.addProduct(product, 20);
+        boolean stock = inventoryService.isLowStock(product);
+        assertFalse(stock);
     }
 }
