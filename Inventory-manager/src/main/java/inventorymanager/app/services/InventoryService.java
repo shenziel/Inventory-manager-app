0ccabaea876baf6455aa44f4c8d7ea0862f2f454
zotzo <hhhjejuas@gmail.com>
2026-02-26 23:15:25 +0100

    added methods to inventoryservice

--- Inventory-manager/src/main/java/inventorymanager/app/services/InventoryService.java
+++ Inventory-manager/src/main/java/inventorymanager/app/services/InventoryService.java
@@ -3,4 +3,9 @@ import inventorymanager.app.Product;
 
 public interface InventoryService {
 //later when tests are done
+    void addProduct(Product product, int quantity);
+    Product getProduct(String id);
+    boolean removeProduct(String id);
+    Product updateProduct(String id, Product updated);
+    boolean isLowStock(Product product);
 }
